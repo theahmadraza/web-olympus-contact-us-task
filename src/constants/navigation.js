0b4f36d16caf7d73236f1f41700dae3c0c5a7f6c
Muhammad Ahamd Raza <60704740+theahmadraza@users.noreply.github.com>
2024-02-28 16:46:39 +0500

    remove some code

--- src/constants/navigation.js
+++ src/constants/navigation.js
@@ -1,5 +1,5 @@
 export const navigation = [
-    { name: 'SERVICES', href: '#' },
-    { name: 'PORTFOLIO', href: '#' },
-    { name: 'ABOUT US', href: '#' },
+    { name: 'SERVICES', href: '/' },
+    { name: 'PORTFOLIO', href: '/' },
+    { name: 'ABOUT US', href: '/' },
   ]
